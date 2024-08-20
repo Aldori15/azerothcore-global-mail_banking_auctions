153b1838452a46cfe7fc2a0675bfcfb43b48138a
Aldori <m.baxter1122@yahoo.com>
2024-08-20 12:54:31 -0400

    Add files via upload

--- lua_scripts/Global Mail_Banking_Auctions.lua
+++ lua_scripts/Global Mail_Banking_Auctions.lua
@@ -1,29 +1,14 @@
 -- ----------------------------------------------------------------------------------------------------------
---  While in-game, just pre-pend any of the commands below with "!" or "."  For example, ".bank" or "!mail".
---  You can also type "#commands" or "$commands" to display the list of available commands.
+--  While in-game, just prepend any of the commands below with "!" or "."  For example, ".bank" or "!mail".
 --
---  Hosted by Aldori15 on Github: https://github.com/Aldori15/global-mail_banking_auctions 
+--  Hosted by Aldori15 on Github: https://github.com/Aldori15/global-mail_banking_auctions
 -- ----------------------------------------------------------------------------------------------------------
 
--- Table to store list of possible commands
 local commands = {
-    bank = { "bank", "openbank" },  -- Chat variations for opening the bank
-    mail = { "mail", "mailbox" },  -- Chat variations for opening the mailbox
-    auction = { "auctions", "ah", "auctionhouse" },  -- Chat variations for opening the auction house
+    bank = { "bank", "openbank" },  -- Chat commands for opening the bank
+    mail = { "mail", "mailbox" },  -- Chat commands for opening the mailbox
+    auction = { "auctions", "ah", "auctionhouse" },  -- Chat commands for opening the auction house
 }
-
-local function DisplayChatCommandList(event, unit, message, type, language)
-    -- Convert message to lowercase for case-insensitive comparison
-    local normalizedMessage = string.lower(message)
-
-	if(normalizedMessage == "#commands") or (normalizedMessage == "$commands") then
-	    unit:SendAreaTriggerMessage("|cFF00B0E8.mail  -  opens the global Mailbox|r")
-        unit:SendAreaTriggerMessage("|cFF00B0E8.bank  -  opens the global Bank|r")
-	    unit:SendAreaTriggerMessage("|cFF00B0E8.auctions  -  opens the global Auction House|r") 
-	end
-    -- Return false to prevent further processing
-	return false
-end
 
 local function HandleChatCommand(event, player, message, type, language)
     -- Convert message to lowercase for case-insensitive comparison
@@ -46,5 +31,4 @@ local function HandleChatCommand(event, player, message, type, language)
     end
 end
 
-RegisterPlayerEvent(18, DisplayChatCommandList)
 RegisterPlayerEvent(42, HandleChatCommand)
