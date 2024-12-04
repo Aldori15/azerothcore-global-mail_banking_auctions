08d5c612f1dced85b6d8f1e1eff3fb69abe6b7db
Aldori <m.baxter1122@yahoo.com>
2024-12-04 10:36:18 -0500

    Added config options to toggle Mail/Bank/Auctions individually

--- lua_scripts/Global Mail_Banking_Auctions.lua
+++ lua_scripts/Global Mail_Banking_Auctions.lua
@@ -5,9 +5,15 @@
 --  Hosted by Aldori15 on Github: https://github.com/Aldori15/global-mail_banking_auctions
 -- ----------------------------------------------------------------------------------------------------------
 
+local config = {
+    enableBank = true,
+    enableMail = true,
+    enableAuction = true,
+}
+
 local commands = {
     bank = { "bank", "openbank" },  -- Chat commands for opening the bank
-    mail = { "mail", "mailbox" },  -- Chat commands for opening the mailbox
+    mail = { "mb", "mail", "mailbox" },  -- Chat commands for opening the mailbox
     auction = { "auctions", "ah", "auctionhouse" },  -- Chat commands for summoning the auction house NPC
 }
 
@@ -26,7 +32,7 @@ local auctioneerNpcEntries = {
 }
 
 local distanceInFront = 1.5  -- Distance to spawn auctioneer in front of the player
-local auctioneerLifespan = 60000  -- 60 seconds lifespan before despawning
+local auctioneerLifespan = 60000  -- 60 second lifespan before despawning
 
 local function HandleAuctionCommand(player)
     -- Calculate spawn position and orientation
@@ -39,7 +45,8 @@ local function HandleAuctionCommand(player)
     local tempAuctioneerEntry = player:GetTeam() == 0 and auctioneerNpcEntries.ALLIANCE or auctioneerNpcEntries.HORDE
     local spawnType = 3  -- TEMPSUMMON_TIMED_DESPAWN.  Do not change this value from 3
     local auctioneer = player:SpawnCreature(tempAuctioneerEntry, spawnX, spawnY, playerZ, spawnO, spawnType, auctioneerLifespan)
-    if auctioneer then
+    
+    if auctioneer then       
         local randomMessage = auctioneerSayMessages[math.random(#auctioneerSayMessages)]
         auctioneer:SendUnitSay(randomMessage, 0)
     else
@@ -54,12 +61,16 @@ local function HandleChatCommand(event, player, message, type, language)
     for action, commandList in pairs(commands) do
         for _, command in ipairs(commandList) do
             if normalizedMessage == command then
-                if action == "bank" then
+                if action == "bank" and config.enableBank then
                     player:SendShowBank(player)
-                elseif action == "mail" then
-                    player:SendShowMail(player)
-                elseif action == "auction" then
+                elseif action == "mail" and config.enableMail then
+                    playerGuid = player:GetGUIDLow()
+                    player:SendShowMailBox(playerGuid)
+                elseif action == "auction" and config.enableAuction then
                     HandleAuctionCommand(player)
+                else
+                    -- If the config is disabled for a specific command, notify the player
+                    player:SendBroadcastMessage("The " .. action .. " command is currently disabled.")
                 end
                 return false  -- Prevent further processing if command is matched
             end
